df5e23a31af5608945c83cdaf37a947ff48d9218
Anthony Lum <anthonylum1@hotmail.com>
2021-09-13 23:50:45 -0400

    Shapes & Colors

---
+++ template-p5-project/js/script.js
@@ -0,0 +1,20 @@
+/*
+Template p5 project
+Anthony Lum
+*/
+
+/*Function to setup space*/
+function setup() {
+  createCanvas(500, 500);
+
+  background(200, 200, 200);
+
+  ellipseMode(CENTER);
+  ellipse(250, 250, 100, 100);
+  ellipse(250, 250, 80, 80);
+
+}
+
+
+/*Function to execute draw*/
+function draw() {}
