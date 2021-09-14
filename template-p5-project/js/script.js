f60ee5623cc78a595133e1e1929bba23e9924010
Anthony Lum <anthonylum1@hotmail.com>
2021-09-14 00:24:34 -0400

    Drawing experiment: adding color

--- template-p5-project/js/script.js
+++ template-p5-project/js/script.js
@@ -8,11 +8,17 @@ function setup() {
   createCanvas(500, 500);
 
   background(200, 200, 200);
+  noStroke();
 
-  ellipseMode(CENTER);
+  ellipseMode(CORNER);
+  fill(127, 0, 100);
   ellipse(250, 250, 100, 100);
+  fill(137, 0, 100);
   ellipse(250, 250, 80, 80);
-
+  fill(147, 0, 100);
+  ellipse(250, 250, 60, 60);
+  fill(157, 0, 100);
+  ellipse(250, 250, 40, 40);
 }
 
 
