9fb3f512808b519f9cd444e33eb0cab01d912d8e
Anthony Lum <anthonylum1@hotmail.com>
2021-09-21 02:25:55 -0400

    Learning about defining variables.

--- practice/practice3/js/script.js
+++ practice/practice3/js/script.js
@@ -5,18 +5,19 @@ Anthony Lum
 Practicing and following along with the Youtube tutorials at 2:30am.
 */
 
-/**
-Description of setup
-*/
+// Variables
+let backgroundShade = 0;
+let circleX = 250;
+let circleY = 250;
+let circleSize = 200;
+
+/** Description of setup */
 function setup() {
-  createCanvas(windowWidth , windowHeight);
-
+  createCanvas(windowWidth, windowHeight);
 }
-
 
 /** Where the action is in p5. Gets run once per frame, 60 times per second */
 function draw() {
-  background(255, 0, 0);
-  rectMode(CENTER);
-  rect(mouseX, mouseY, 100, 100);
+  background(backgroundShade);
+  ellipse(circleX, circleY, circleSize);
 }
