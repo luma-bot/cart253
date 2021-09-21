e5c5d5a7f0c3c387642050f3bdaf01c4039755cb
Anthony Lum <anthonylum1@hotmail.com>
2021-09-21 14:10:23 -0400

    Grand Update

---
+++ practice/class_practice3/js/script.js
@@ -0,0 +1,30 @@
+/*
+In class Practice 09/21
+Anthony Lum
+
+Practicing and following along with the class
+
+/* Variables */
+  let rectangle = {
+    x: 0,
+    y: 250,
+    size: 50,
+    speed: 10,
+  };
+
+/** Description of setup */
+function setup() {
+  createCanvas(500,500);
+}
+
+/** Where the action is in p5. Gets run once per frame, 60 times per second */
+function draw() {
+  background(0);
+
+  noFill();
+  stroke(255);
+  rectMode(CENTER);
+  rect(rectangle.x, rectangle.y, rectangle.size);
+
+  rectangle.y -= rectangle.speed;
+}
