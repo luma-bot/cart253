a3eacb6f7b627f673ab4ed5cebc9c943d2387c4c
Anthony Lum <anthonylum1@hotmail.com>
2021-09-21 02:35:23 -0400

    Learning about +=

--- practice/practice3/js/script.js
+++ practice/practice3/js/script.js
@@ -7,9 +7,11 @@ Practicing and following along with the Youtube tutorials at 2:30am.
 
 // Variables
 let backgroundShade = 0;
-let circleX = 250;
+let circleX = 0;
 let circleY = 250;
 let circleSize = 200;
+let circleSpeed = 2; // neg = left, pos = right
+let circleAcceleration = 0.25;
 
 /** Description of setup */
 function setup() {
@@ -19,5 +21,8 @@ function setup() {
 /** Where the action is in p5. Gets run once per frame, 60 times per second */
 function draw() {
   background(backgroundShade);
+  circleX += circleSpeed;
+  // is the same as circleX = circleX + circleSpeed;
+  circleSpeed += circleAcceleration;
   ellipse(circleX, circleY, circleSize);
 }
