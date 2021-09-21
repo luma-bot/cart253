7ab3b7feb43f8353778da8c53e8984b3c9f4877b
Anthony Lum <anthonylum1@hotmail.com>
2021-09-21 17:13:40 -0400

    E1: Started exercise 1
    Tempalte copied from Activity 3

--- exercise/i_like_to_move_it/js/script.js
+++ exercise/i_like_to_move_it/js/script.js
@@ -5,65 +5,26 @@ Anthony Lum
 I like to move it move it, you like to move it move it, we like to move it!
 
 PLAN:
-- Figure out what variables we need
-- Set up the canvas
-- Set the background color, remembering it needs to change over time
-- Deal with the left circle coming onto the screen, stopping in the middle, and growing
-- Deal with the right circle doing the same thing
+- Continue to get comfortable using Git and GitHub when you work on new projects
+- Declaring and changing variables (especially JavaScript objects!)
+
+- Include three shapes
+- Include movement
+- Include size changes
+- Include color changes
+- Use map() and constrain()
+- Respond to the mouse position using mouseX and mouseY
 */
 
 //Variable setup
-let bg = {
-  r: 0,
-  g: 0,
-  b: 0,
-};
 
-let circle1 = {
-  x: 0,
-  y: 250,
-  size: 100,
-  growthRate: 1,
-  speed: 1,
-  fill: 255,
-  alpha: 200,
-};
-
-let circle2 = {
-  x: 500,
-  y: 250,
-  size: 75,
-  sizeRatio: 0.75,
-  speed: -1,
-  fill: 255,
-  alpha: 200,
-};
 
 //It's a trap! No it's a setup.
 function setup() {
-  createCanvas(500, 500);
-  noStroke();
+
 }
 
 // Drawing Time
 function draw() {
-  //Background
-  background(bg.r, bg.g, bg.b);
-  bg.r = map(circle1.size,100,width,0,255);
-  // When circle1.size is = 100, color = 0, when circle1.size = width, color = 255.
 
-  //Left Circle
-  circle1.x += circle1.speed;
-  circle1.x = constrain(circle1.x, 0, width / 2); // circle1.x, Lowest Value, Highest Value
-  circle1.size += circle1.growthRate;
-  circle1.size = constrain(circle1.size,0,width); // circle1.size, Lowest Value, Highest Value
-  fill(circle1.fill, circle1.alpha);
-  ellipse(circle1.x, circle1.y, circle1.size);
-
-  //Right Circle
-  circle2.x += circle2.speed;
-  circle2.x = constrain(circle2.x, width / 2, width);
-  circle2.size = circle1.size * circle2.sizeRatio;
-  fill(circle2.fill, circle2.alpha);
-  ellipse(circle2.x, circle1.y, circle2.size);
 }
