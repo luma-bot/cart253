6f8b87c79fc6815d3eebe7271fdd71ec787d9e0b
Anthony Lum <anthonylum1@hotmail.com>
2021-09-21 02:44:13 -0400

    Creating an object

--- practice/practice3/js/script.js
+++ practice/practice3/js/script.js
@@ -7,11 +7,18 @@ Practicing and following along with the Youtube tutorials at 2:30am.
 
 // Variables
 let backgroundShade = 0;
-let circleX = 0;
-let circleY = 250;
-let circleSize = 200;
-let circleSpeed = 2; // neg = left, pos = right
-let circleAcceleration = 0.25;
+// let circleX = 0;
+// let circleY = 250;
+// let circleSize = 200;
+// let circleSpeed = 2; // neg = left, pos = right
+// let circleAcceleration = 0.25;
+
+let circle = {
+  x:0,
+  y:250,
+  size: 200,
+  speed: 2,
+};
 
 /** Description of setup */
 function setup() {
@@ -21,8 +28,7 @@ function setup() {
 /** Where the action is in p5. Gets run once per frame, 60 times per second */
 function draw() {
   background(backgroundShade);
-  circleX += circleSpeed;
+  circle.x += circle.speed;
   // is the same as circleX = circleX + circleSpeed;
-  circleSpeed += circleAcceleration;
-  ellipse(circleX, circleY, circleSize);
+  ellipse(circle.x, circle.y, circle.size);
 }
