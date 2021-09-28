75023435948b14283236693d202e3c63f934fc4b
Anthony Lum <anthonylum1@hotmail.com>
2021-09-27 23:00:38 -0400

    Starting 4.8

--- practice/practice4/js/script.js
+++ practice/practice4/js/script.js
@@ -10,40 +10,54 @@ if (homework.start >= time.midnight){
 */
 
 
-let backgroundShade = 0;
-let circle = {
-  x: 250,
-  y: 250,
-  size: 100,
-  vx: 0,
-  vy: 0,
-  ax: 0,
-  ay: 0,
-  speed: 5,
-}
+// let backgroundShade = 0;
+// let circle = {
+//   x: 250,
+//   y: 250,
+//   size: 100,
+//   vx: 0,
+//   vy: 0,
+//   ax: 0,
+//   ay: 0,
+//   speed: 5,
+// }
+//
+// function setup() {
+//   createCanvas(500, 500);
+// }
+//
+// function draw() {
+//   background(backgroundShade);
+//
+//
+//   if (mouseX < circle.x) {
+//     circle.vx = -circle.speed;
+//   } else {
+//     circle.vx = circle.speed;
+//   }
+//
+//   if (mouseY < circle.y) {
+//     circle.vy = -circle.speed;
+//   } else {
+//     circle.vy = circle.speed;
+//   }
+//
+//
+//   circle.x += circle.vx;
+//   circle.y += circle.vy;
+//   ellipse(circle.x, circle.y, circle.size);
+// }
 
 function setup() {
   createCanvas(500, 500);
 }
 
+
 function draw() {
-  background(backgroundShade);
+  background(0);
 
-
-  if (mouseX < circle.x) {
-    circle.vx = -circle.speed;
-  } else {
-    circle.vx = circle.speed;
-  }
-
-  if (mouseY < circle.y) {
-    circle.vy = -circle.speed;
-  } else {
-    circle.vy = circle.speed;
-  }
-
-
-  circle.x += circle.vx;
-  circle.y += circle.vy;
-  ellipse(circle.x, circle.y, circle.size);
+  fill(255,0,0);
+  stroke(0,255,255);
+  strokeWeight(10);
+  rect(100,100,100,100);
 }
