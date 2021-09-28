48fffdc979b8b42a5cd51ad79c725ed05c077c1c
Anthony Lum <anthonylum1@hotmail.com>
2021-09-27 23:20:56 -0400

    Completed Intermediate drawing

--- practice/practice4/js/script.js
+++ practice/practice4/js/script.js
@@ -48,6 +48,9 @@ if (homework.start >= time.midnight){
 //   ellipse(circle.x, circle.y, circle.size);
 // }
 
+let angle = 0;
+let rectScale = 0;
+
 function setup() {
   createCanvas(500, 500);
 }
@@ -56,8 +59,41 @@ function setup() {
 function draw() {
   background(0);
 
+  // push(); // save
+  // fill(255, 0, 0);
+  // stroke(0, 255, 255);
+  // strokeWeight(10);
+  // rect(100, 100, 100, 100);
+  // pop(); // reset
+  //
+  // push();
+  // fill(0, 0, 255);
+  // rect(300, 100, 100, 100);
+  // pop();
+
   fill(255,0,0);
-  stroke(0,255,255);
-  strokeWeight(10);
-  rect(100,100,100,100);
+  rect(0,0,100,100);
+
+push();
+  translate(200,200);
+  fill(255,0,0);
+  translate(width/2,height/2);
+  rectMode(CENTER);
+  rotate(angle);
+  scale(rectScale);
+  rect(0,0,100,100);
+pop();
+
+angle += 0.01;
+rectScale += 0.01;
+
+  translate(200,200); // add 200
+  fill(0,255,0);
+  rect(0,0,100,100);
+
+  translate(0,200); // add 200 from the last one, additive
+  fill(0,255,0);
+  rect(0,0,100,100);
+
+  //use push pop to stop translate so they don't add up
 }
