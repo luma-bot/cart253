31e4c96dcf7591e7f36a2fffcc860d8dc9981872
Anthony Lum <anthonylum1@hotmail.com>
2021-09-28 02:59:01 -0400

    Added Covid Text

--- exercise/dodge_em/js/script.js
+++ exercise/dodge_em/js/script.js
@@ -51,6 +51,20 @@ let user = {
   }
 };
 
+// text
+let text1 = {
+  x: 200,
+  y: 150,
+  size: 25,
+  padding: 150,
+  fill: {
+    r: 255,
+    g: 255,
+    b: 255
+  }
+
+}
+
 let numStatic = 1000;
 
 let covid19Image;
@@ -73,17 +87,18 @@ function setup() {
   // User Spawn
   user.x = windowWidth / 2 + 200;
   user.y = windowHeight / 2;
-
 } /** End of setup() */
 
 
 /** Displaying the simulation */
 function draw() {
-  if (user.x > windowWidth || user.x < 0 || user.y > windowHeight || user.y < 0) {
-    background(255, 0, 0); // green
-  } else {
-    background(0); // black
-  }
+  // if (user.x > windowWidth || user.x < 0 || user.y > windowHeight || user.y < 0) {
+  //   background(255, 0, 0); // green
+  // } else {
+  //   background(0); // black
+  // }
+
+  background(0); // black
 
   // Display Static, wrap in for loop, decorative static
   for (let i = 0; i < numStatic; i++) {
@@ -120,24 +135,6 @@ function draw() {
     user.y += user.down;
   }
 
-  // Check for catching covid19
-  // object.size = diameter, object.size/2 = radius
-  let distance = dist(user.x, user.y, covid19.x, covid19.y); // only need now and won't need it later, declaring where we need it
-  if (distance < covid19.size / 2 + user.size / 2) {
-    // noLoop();
-    console.log(distance);
-    user.size = user.size - user.shrink;
-    covid19.size = covid19.size + covid19.grow;
-  }
-
-  if (user.size <= 0) {
-    noLoop();
-  } else {
-    covid19.size = covid19.size + covid19.passiveGrow;
-    covid19.speed = covid19.speed + covid19.passiveGrow;
-  }
-
-
   // Display Covid 19
   imageMode(CENTER);
   image(covid19Image, covid19.x, covid19.y, covid19.size, covid19.size);
@@ -148,4 +145,46 @@ function draw() {
   imageMode(CENTER);
   image(userImage, user.x, user.y, user.size, user.size);
   // fill(user.fill.r, user.fill.g, user.fill.b);
+
+  // Contrain user
+  user.x = constrain(user.x, 0, windowWidth);
+  user.y = constrain(user.y, 0, windowHeight);
+
+
+  // Check for catching covid19
+  // object.size = diameter, object.size/2 = radius
+  let distance = dist(user.x, user.y, covid19.x, covid19.y); // only need now and won't need it later, declaring where we need it
+  if (distance < covid19.size / 2 + user.size / 2) {
+    // noLoop();
+    console.log(distance);
+    user.size = user.size - user.shrink;
+    covid19.size = covid19.size + covid19.grow;
+  }
+
+  // Passive Game End
+  if (user.size <= 0) {
+    // Text render
+    fill(text1.fill.r, fill.g, fill.b);
+    textSize(text1.size);
+    textStyle(BOLD);
+    text('You caught Covid19', user.x + text1.padding, user.y + text1.padding); // right
+
+    fill(text1.fill.r, fill.g, fill.b);
+    textSize(text1.size);
+    textStyle(BOLD);
+    text('You caught Covid19', user.x - text1.padding, user.y - text1.padding); // left
+
+    fill(text1.fill.r, fill.g, fill.b);
+    textSize(text1.size);
+    textStyle(BOLD);
+    text('You caught Covid19', windowWidth / 2, windowHeight / 2); // center
+
+    noLoop();
+
+  } else {
+    covid19.size = covid19.size + covid19.passiveGrow;
+    covid19.speed = covid19.speed + covid19.passiveGrow;
+  }
+
+
 } /** End of draw() */
