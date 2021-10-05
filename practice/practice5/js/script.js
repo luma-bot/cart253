492b687a8f3986999ffab3d67515676c8b930dab
Anthony Lum <anthonylum1@hotmail.com>
2021-10-04 23:58:21 -0400

    Starting Practice 5.1 to 5.5

---
+++ practice/practice5/js/script.js
@@ -0,0 +1,31 @@
+/**
+Practice 5, 5.1 - 5.5
+
+Another homework night at 11:57pm.
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
