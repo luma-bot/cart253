fd90ac124a87f579775394183abd0a2201b12e45
Anthony Lum <anthonylum1@hotmail.com>
2021-10-11 11:56:08 -0400

    P6: Finished 6.1-6.2

--- practice/practice6/js/script.js
+++ practice/practice6/js/script.js
@@ -1,19 +1,17 @@
 /**
-Title of Project
-Author Name
-
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Practice 6.1 - 6.2
+Anthony Lum
 */
 
 "use strict";
 
+let barkSFX;
 
 /**
 Description of preload
 */
 function preload() {
-
+  barkSFX = loadSound('assets/sounds/bark.wav');
 }
 
 
@@ -21,7 +19,7 @@ function preload() {
 Description of setup
 */
 function setup() {
-
+  createCanvas(500,500);
 }
 
 
@@ -29,5 +27,17 @@ function setup() {
 Description of draw()
 */
 function draw() {
+  background(0);
+}
 
+function mousePressed(){
+  if (!barkSFX.play()){
+    music.loop();
+  }
+}
+
+function keyPressed(){
+  if (!barkSFX.play()){
+    music.loop();
+  }
 }
