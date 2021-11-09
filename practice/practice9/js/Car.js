b74431bd94aaf36b2d676e406c20c6767a938fe8
Anthony Lum <55846647+luma-bot@users.noreply.github.com>
2021-11-09 01:50:28 -0500

    P9: Overridng methods, drunk car driving

--- practice/practice9/js/Car.js
+++ practice/practice9/js/Car.js
@@ -2,12 +2,36 @@
 Car subClass
 */
 
-class Car extends Vehicle{
+class Car extends Vehicle {
   constructor(x, y) {
-    super(x,y);
+    super(x, y);
     this.width = 50;
     this.height = 20;
     this.vx = 5; // Right
+    this.drunk = 0.2; // 20% of the time
+  }
+
+  move() { // overriding method, use super to call the rest
+    this.veer(); // specific to Car.js
+    super.move(); // called the super
+  }
+
+  veer() {
+    let r = random();
+    if (r < this.drunk) {
+      this.vy = random(-5, 5); // move it down or up
+    }
+  }
+
+  wrap() { // overriding method, use super to call the rest
+    super.wrap(); // called the super
+
+    if (this.y > height){
+      this.y -= height;
+    }
+    else if (this.y < 0){
+      this.y += height;
+    }
   }
 
   display() {
