5ce8254619ed840e51ab532235a7ae93fc372edc
Anthony Lum <55846647+luma-bot@users.noreply.github.com>
2021-11-09 23:24:16 -0500

    FP: Can move the coffee around

--- projects/project2/prototype/js/script.js
+++ projects/project2/prototype/js/script.js
@@ -10,6 +10,13 @@ Try not to mess up people's orders else they'll get mad. Be quick and efficient,
 
 "use strict";
 
+let coffeeX;
+let coffeeY;
+let coffeeBoxSize = 75;
+let coffeeoverBox = false;
+let coffeeLocked = false;
+let coffeeXoffset = 0.0;
+let coffeeYoffset = 0.0;
 
 /**
 Description of preload
@@ -24,18 +31,66 @@ Description of setup
 */
 function setup() {
   createCanvas(600, 600);
-}
+  coffeeX = width / 2.0;
+  coffeeY = height / 2.0;
 
+  rectMode(RADIUS);
+  strokeWeight(2);
+}
 
 /**
 Description of draw()
 */
 function draw() {
   background(0);
-  let coffee = new Coffee();
-  
-  coffee.display();
-  coffee.mousePressed();
-  coffee.mouseDragged();
-  coffee.mouseReleased();
+
+  coffee();
 }
+
+// Coffee Functions Start
+function coffee() {
+
+  // Test if the cursor is over the box
+  if (
+    mouseX > coffeeX - coffeeBoxSize &&
+    mouseX < coffeeX + coffeeBoxSize &&
+    mouseY > coffeeY - coffeeBoxSize &&
+    mouseY < coffeeY + coffeeBoxSize
+  ) {
+    coffeeoverBox = true;
+    if (!coffeeLocked) {
+      stroke(255);
+      fill(94, 68, 47);
+    }
+  } else {
+    stroke(170, 123, 85);
+    fill(94, 68, 47);
+    coffeeoverBox = false;
+  }
+
+  // Draw the box
+  rect(coffeeX, coffeeY, coffeeBoxSize, coffeeBoxSize);
+}
+
+function mousePressed() {
+  if (coffeeoverBox) {
+    coffeeLocked = true;
+    fill(255, 255, 255);
+  } else {
+    coffeeLocked = false;
+  }
+  coffeeXoffset = mouseX - coffeeX;
+  coffeeYoffset = mouseY - coffeeY;
+}
+
+function mouseDragged() {
+  if (coffeeLocked) {
+    coffeeX = mouseX - coffeeXoffset;
+    coffeeY = mouseY - coffeeYoffset;
+  }
+}
+
+function mouseReleased() {
+  coffeeLocked = false;
+}
+// Coffee Functions End
