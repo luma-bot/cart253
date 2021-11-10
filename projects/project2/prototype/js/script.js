db2f06c941710e6f224c2f6c336936ca672ef139
Anthony Lum <55846647+luma-bot@users.noreply.github.com>
2021-11-09 22:16:04 -0500

    FP: Starting initial js files.

--- projects/project2/prototype/js/script.js
+++ projects/project2/prototype/js/script.js
@@ -1,9 +1,11 @@
-/**
-Title of Project
-Author Name
+/*
+P2: Good Bean Water
+Anthony Lum, 40098555
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+A barista simulator.
+
+You are a newly hired barista working at "Good Bean Water". They didn't really give you much training to prepare you for your first shift, so there will be a lot of learning on the job!
+Try not to mess up people's orders else they'll get mad. Be quick and efficient, and get those orders out so you can get paid!
 */
 
 "use strict";
@@ -21,7 +23,7 @@ function preload() {
 Description of setup
 */
 function setup() {
-
+  createCanvas(600, 600);
 }
 
 
@@ -29,5 +31,11 @@ function setup() {
 Description of draw()
 */
 function draw() {
-
+  background(0);
+  let coffee = new Coffee();
+  
+  coffee.display();
+  coffee.mousePressed();
+  coffee.mouseDragged();
+  coffee.mouseReleased();
 }
