f57a7afd9831cf19288c7a91d64a44895692d17f
Anthony Lum <55846647+luma-bot@users.noreply.github.com>
2021-11-16 21:07:51 -0500

    PR: Starting folder and files organization process.

---
+++ projects/project2/final/js/script.js
@@ -0,0 +1,36 @@
+/*
+P2: Good Bean Water
+Anthony Lum, 40098555
+
+A barista simulator.
+You are a newly hired barista working at "Good Bean Water". They didn't really give you much training to prepare you for your first shift, so there will be a lot of learning on the job!
+Try not to mess up people's orders else they'll get mad. Be quick and efficient, and get those orders out so you can get paid!
+*/
+
+// -----------------------------------------------------------------------------
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
